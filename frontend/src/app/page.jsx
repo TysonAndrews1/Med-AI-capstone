fffee80387aa8c8d11897eaedf5ad38270097362
TysonAndrews1 <tyson.andrews@edu.sait.ca>
2025-03-11 09:22:54 -0600

    cleared default page

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import react from "react"
+export default function Home() {
+  return (
+    <div className="white">
+     <header>
+      <h1>sure</h1>
+     </header>
+    </div>
+  );
+}
