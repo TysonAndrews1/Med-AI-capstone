e1a9c523fb8590029cb46fc4cee10b29edfe9f94
TysonAndrews1 <tyson.andrews@edu.sait.ca>
2025-03-13 10:18:33 -0600

    Test UI

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer(){
+    return(
+        <div className="bg-[#1B4D3E] w-full h-16 flex flex-col items-center text-white">
+            <p>all rights reservered or whatever</p>
+            <p>and other stuff</p>
+
+
+        </div>
+    )
+}
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Header(){
+
+    return (
+        <div className="flex justify-between items-center p-4  rounded-lg shadow-md w-full h-[18%]">
+          {/* Centered Content - Icon, Title, Subtitle */}
+          <div className="flex items-center space-x-4 mx-auto bg-custom-color">
+            {/* <icon className="w-6 h-6" /> Adjust icon size as needed */}
+            <div className="text-center">
+              <p className="text-5xl font-semibold text-[#1B4D3E] ">MediHealth Ai</p>
+              <p className="text-xl text-[#1B4D3E]">Your #1 at Home Heath Companion</p>
+            </div>
+          </div>
+      
+          {/* Button - Right Aligned */}
+          <button className="flex items-center space-x-2 px-4 py-2 bg-[#1B4D3E] text-white rounded-lg hover:">
+            <p>Start Health Assessment</p>
+            {/* <icon className="w-5 h-5" /> */}
+          </button>
+        </div>
+      );
+        
+}
