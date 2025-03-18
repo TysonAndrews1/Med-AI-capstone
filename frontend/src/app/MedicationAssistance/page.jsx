9d7b58c41787c573d503e09edc611a30b18a89cb
TysonAndrews1 <tyson.andrews@edu.sait.ca>
2025-03-18 07:56:36 -0600

    backend for free drug api call

--- frontend/src/app/MedicationAssistance/page.jsx
+++ frontend/src/app/MedicationAssistance/page.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+"use client"
+
+import React, { useEffect, useState } from "react";
+import {getDrugId} from "./APICallsDrug"
+import MedicineForm from "./MedicinePreForm";
 
 export default function page(){
-    return(<></>)
+    const medicineCode = "00000086"; //Testing only
+    const [request, setRequest] = useState("")
+      useEffect(() => {
+            getDrugId(medicineCode).then((e)=>{setRequest(e)});
+        }, []);
+    return(
+    
+    
+    <div>
+        <MedicineForm/>        
+        <p>{JSON.stringify( request)}</p>
+
+    </div>)
 }
