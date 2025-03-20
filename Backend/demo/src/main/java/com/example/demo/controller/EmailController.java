db853c35c395098f07b03b36f72105e7bb313a74
TysonAndrews1 <tyson.andrews@edu.sait.ca>
2025-03-19 23:33:39 -0600

    credit given for tutorial used

--- Backend/demo/src/main/java/com/example/demo/controller/EmailController.java
+++ Backend/demo/src/main/java/com/example/demo/controller/EmailController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.service.EmailService;;
 
+//  Tutporial used for the framework code https://mailtrap.io/blog/spring-send-email/#How-to-send-emails-from-Spring-Boot-using-Gmail-SMTP
+
 @RestController
 @RequestMapping("/email")
 public class EmailController {
