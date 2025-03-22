1a8b0b0df8d5615b3ea962cb25c344126e1bfb2d
Michellepotran <113163108+Michellepotran@users.noreply.github.com>
2025-03-22 07:13:09 -0600

    Display google maps

--- frontend/src/app/FindADoctor/components/GoogleMap.jsx
+++ frontend/src/app/FindADoctor/components/GoogleMap.jsx
@@ -2,21 +2,26 @@
 
 import { useState } from "react";
 import {
-    APIProvider,
-    Map, 
-    AdvancedMarker, 
-    pin, 
-    InfoWindow,
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  pin,
+  InfoWindow,
 } from "@vis.gl/react-google-maps";
 
+
 function GoogleMap() {
-    const position = { lat: 53.54, lng: 10 };
+  const position = { lat: 53.54, lng: 10 };
 
-    return (<APIProvider>
-        <div>React Google Maps</div>
-    </APIProvider>);
+  console.log("Google Maps API Key:", process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY);
 
-
+  return (
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+      <div style={{ height: "100vh" }}>
+        <Map zoom={9} center={position}></Map>
+      </div>
+    </APIProvider>
+  );
 }
 
 export default GoogleMap;
