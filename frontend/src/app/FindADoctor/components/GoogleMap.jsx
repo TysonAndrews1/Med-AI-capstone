219e5c132b9187389ad5cb455f295b366d131508
Michellepotran <113163108+Michellepotran@users.noreply.github.com>
2025-03-22 07:30:59 -0600

    Add marker to map

--- frontend/src/app/FindADoctor/components/GoogleMap.jsx
+++ frontend/src/app/FindADoctor/components/GoogleMap.jsx
@@ -17,8 +17,14 @@ function GoogleMap() {
 
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-      <div style={{ height: "100vh" }}>
-        <Map zoom={9} center={position}></Map>
+      <div style={{ height: "100vh", width: "100%" }}>
+        <Map zoom={9} center={position} mapId={process.env.NEXT_PUBLIC_MAP_ID}></Map>
+        <AdvancedMarker
+          position={position}
+          icon={pin}
+          onClick={() => console.log("Marker clicked")}>
+
+          </AdvancedMarker>
       </div>
     </APIProvider>
   );
